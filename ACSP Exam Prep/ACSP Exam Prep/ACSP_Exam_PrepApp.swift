910e63a8d837f93ade04d2254fa6efb6d228aeb6
Craig Opie <craigopie@gmail.com>
2023-08-14 17:20:57 -1000

    take 2

---
+++ ACSP Exam Prep/ACSP Exam Prep/ACSP_Exam_PrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACSP_Exam_PrepApp.swift
+//  ACSP Exam Prep
+//
+//  Created by Craig on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ACSP_Exam_PrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ACSP Exam Prep/ACSP Exam Prep/Question.swift
@@ -0,0 +1,55 @@
+//
+//  Question.swift
+//  ACSP Exam Prep
+//
+//  Created by Craig on 8/14/23.
+//
+
+import Foundation
+
+struct Question: Identifiable, Decodable {
+    private(set) var id: UUID = UUID()
+    let question: String
+    let answers: Answer
+    var allOptions: [String]
+
+    struct Answer: Decodable {
+        let correct: [String]
+        let incorrect: [String]
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.question = try container.decode(String.self, forKey: .question)
+
+        let answersContainer = try container.nestedContainer(keyedBy: AnswerKeys.self, forKey: .answers)
+
+        let correctAnswers: [String]
+        if let singleCorrect = try? answersContainer.decode(String.self, forKey: .correct) {
+            correctAnswers = [singleCorrect]
+        } else {
+            correctAnswers = try answersContainer.decode([String].self, forKey: .correct)
+        }
+
+        let incorrectAnswers = try answersContainer.decode([String].self, forKey: .incorrect)
+        self.answers = Answer(correct: correctAnswers, incorrect: incorrectAnswers)
+
+        var options = answers.incorrect
+        let correctIndex = Int.random(in: 0...answers.incorrect.count)
+        options.insert(contentsOf: answers.correct, at: correctIndex)
+        self.allOptions = options
+    }
+
+    func isCorrectAnswer(_ answer: [String]) -> Bool {
+        return Set(answer) == Set(answers.correct)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case question, answers
+    }
+
+    private enum AnswerKeys: String, CodingKey {
+        case correct, incorrect
+    }
+}
